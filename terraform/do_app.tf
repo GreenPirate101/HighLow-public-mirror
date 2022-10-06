eb7dee713e735960318fd328c0851d344a281675
CyberCitizen01 <77384412+CyberCitizen01@users.noreply.github.com>
2022-10-06 22:07:30 +0530

    [ADD] tf for `functions`

--- terraform/do_app.tf
+++ terraform/do_app.tf
@@ -6,5 +6,23 @@ resource "digitalocean_app" "highlow" {
     alert {
       rule = "DEPLOYMENT_FAILED"
     }
+
+
+    function {
+      name = "highlow-functions"
+      github {
+        repo           = "CyberCitizen01/HighLow"
+        branch         = "main"
+        deploy_on_push = false
+      }
+    }
+  }
+}
+
+output "do_app" {
+  value = {
+    id                   = digitalocean_app.highlow.id
+    default_ingress      = digitalocean_app.highlow.default_ingress
+    live_url             = digitalocean_app.highlow.live_url
   }
 }
